34126699d442671c5e8bf21da61ff742d4556db6
AfsarTanvir <afsartanvir805@gmail.com>
2025-10-28 00:28:10 +0600

    Employee Salaries

---
+++ Hecker_Rank_SQL/Employee Salaries.sql
@@ -0,0 +1,7 @@
+-- Hecker Rank Easy Basic
+-- https://www.hackerrank.com/challenges/salary-of-employees/problem?isFullScreen=true
+-- Employee Salaries
+
+
+SELECT name from employee 
+    where salary > 2000 and months < 10;
