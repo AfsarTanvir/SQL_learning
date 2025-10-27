6814bcaa0f4fd4fe9ba045a8c8784dea905d8135
AfsarTanvir <afsartanvir805@gmail.com>
2025-10-27 22:16:42 +0600

    Revising the Select Query I

---
+++ Hecker_Rank_SQL/Revising the Select Query I.sql
@@ -0,0 +1,7 @@
+-- Hecker Rank Easy Basic
+-- https://www.hackerrank.com/challenges/revising-the-select-query/submissions/code/452311719
+-- Revising the Select Query I
+
+SELECT * FROM CITY 
+    WHERE population > 100000
+    AND countrycode = 'USA';
