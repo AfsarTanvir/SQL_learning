349762d279f53f8c35f8bc6eb1565c2205ffae19
AfsarTanvir <afsartanvir805@gmail.com>
2025-10-27 22:32:09 +0600

    Revising the Select Query II

---
+++ Hecker_Rank_SQL/Revising the Select Query II.sql
@@ -0,0 +1,6 @@
+-- Hecker Rank Easy Basic
+-- https://www.hackerrank.com/challenges/revising-the-select-query-2/problem
+-- Revising the Select Query II
+
+SELECT name FROM CITY 
+    WHERE population > 120000 AND countrycode='USA';
