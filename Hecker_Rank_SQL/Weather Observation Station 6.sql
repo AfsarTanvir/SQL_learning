610ed6a8901e3622cff4a1e17d7fed8c61f12aff
AfsarTanvir <afsartanvir805@gmail.com>
2025-10-28 00:12:25 +0600

    Weather Observation Station 6

---
+++ Hecker_Rank_SQL/Weather Observation Station 6.sql
@@ -0,0 +1,10 @@
+-- Hecker Rank Easy Basic
+-- https://www.hackerrank.com/challenges/weather-observation-station-6/problem?isFullScreen=true
+-- Weather Observation Station 6
+
+select distinct city from station 
+    where city like 'a%'
+    or city like 'e%' 
+    or city like 'i%' 
+    or city like 'o%' 
+    or city like 'u%'
