3b9c08e2aed3cd243ed87c2ee57dec13d90163dc
AfsarTanvir <afsartanvir805@gmail.com>
2025-10-27 22:41:43 +0600

    Weather Observation Station 1

---
+++ Hecker_Rank_SQL/Weather Observation Station 1.sql
@@ -0,0 +1,5 @@
+-- Hecker Rank Easy Basic
+-- https://www.hackerrank.com/challenges/weather-observation-station-1/problem?isFullScreen=true
+-- Weather Observation Station 1
+
+SELECT city, state from station;
