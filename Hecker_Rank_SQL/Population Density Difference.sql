9a8e3b8082b98e338460953b4a51a182f87d2469
AfsarTanvir <afsartanvir805@gmail.com>
2025-10-28 23:47:00 +0600

    Population Density Difference

---
+++ Hecker_Rank_SQL/Population Density Difference.sql
@@ -0,0 +1,5 @@
+-- Hecker Rank Easy Basic
+-- https://www.hackerrank.com/challenges/population-density-difference/problem?isFullScreen=true
+-- Population Density Difference
+
+SELECT MAX(POPULATION) - MIN(POPULATION) FROM CITY;
