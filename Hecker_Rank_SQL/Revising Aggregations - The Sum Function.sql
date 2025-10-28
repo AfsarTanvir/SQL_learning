6c828a8a674cbcdcd603ef65d7c41227cda08ddf
AfsarTanvir <afsartanvir805@gmail.com>
2025-10-28 23:38:23 +0600

    Revising Aggregations - The Sum Function

---
+++ Hecker_Rank_SQL/Revising Aggregations - The Sum Function.sql
@@ -0,0 +1,5 @@
+-- Hecker Rank Easy Basic
+-- https://www.hackerrank.com/challenges/revising-aggregations-sum/problem?isFullScreen=false
+-- Revising Aggregations - The Sum Function
+
+SELECT SUM(POPULATION) FROM CITY WHERE DISTRICT = 'California';
