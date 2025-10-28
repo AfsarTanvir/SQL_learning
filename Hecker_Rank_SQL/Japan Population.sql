7846243f65c985fd7433ad1ed7875c493f91db4d
AfsarTanvir <afsartanvir805@gmail.com>
2025-10-28 23:45:35 +0600

    Japan Population

---
+++ Hecker_Rank_SQL/Japan Population.sql
@@ -0,0 +1,5 @@
+-- Hecker Rank Easy Basic
+-- https://www.hackerrank.com/challenges/japan-population/problem?isFullScreen=false
+-- Japan Population
+
+SELECT SUM(POPULATION) FROM CITY WHERE COUNTRYCODE = 'JPN';
