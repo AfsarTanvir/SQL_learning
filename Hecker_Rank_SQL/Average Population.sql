6e17055ff93f5132fc925c95879140cd4f09a45d
AfsarTanvir <afsartanvir805@gmail.com>
2025-10-28 23:42:30 +0600

    Average Population

---
+++ Hecker_Rank_SQL/Average Population.sql
@@ -0,0 +1,6 @@
+-- Hecker Rank Easy Basic
+-- https://www.hackerrank.com/challenges/average-population/problem?isFullScreen=false
+-- Average Population
+
+
+SELECT ROUND(AVG(POPULATION)) FROM CITY;
