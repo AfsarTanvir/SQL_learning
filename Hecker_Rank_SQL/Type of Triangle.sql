f4ca54c6669245220158fd61923dd0351f613ddf
AfsarTanvir <afsartanvir805@gmail.com>
2025-10-28 23:32:07 +0600

    Type of Triangle

---
+++ Hecker_Rank_SQL/Type of Triangle.sql
@@ -0,0 +1,12 @@
+-- Hecker Rank Easy Basic
+-- https://www.hackerrank.com/challenges/what-type-of-triangle/problem?isFullScreen=true
+-- Type of Triangle
+
+SELECT 
+  CASE
+    WHEN A + B <= C OR A + C <= B OR B + C <= A THEN 'Not A Triangle'
+    WHEN A = B AND B = C THEN 'Equilateral'
+    WHEN A = B OR B = C OR A = C THEN 'Isosceles'
+    ELSE 'Scalene'
+  END AS TriangleType
+FROM TRIANGLES;
