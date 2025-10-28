a631e133747c98a516261537e6f004fb32a9075c
AfsarTanvir <afsartanvir805@gmail.com>
2025-10-28 23:36:02 +0600

    Revising Aggregations - The Count Function

---
+++ Hecker_Rank_SQL/Revising Aggregations - The Count Function.sql
@@ -0,0 +1,5 @@
+-- Hecker Rank Easy Basic
+-- https://www.hackerrank.com/challenges/revising-aggregations-the-count-function/problem?isFullScreen=false
+-- Revising Aggregations - The Count Function
+
+SELECT COUNT(NAME) FROM CITY WHERE POPULATION > 100000; 
