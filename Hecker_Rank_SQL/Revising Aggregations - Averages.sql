764d42c40d62f973d7b24e3d404e18d012525202
AfsarTanvir <afsartanvir805@gmail.com>
2025-10-28 23:40:08 +0600

    Revising Aggregations - Averages

---
+++ Hecker_Rank_SQL/Revising Aggregations - Averages.sql
@@ -0,0 +1,5 @@
+-- Hecker Rank Easy Basic
+-- https://www.hackerrank.com/challenges/revising-aggregations-the-average-function/problem?isFullScreen=false
+-- Revising Aggregations - Averages
+
+SELECT AVG(POPULATION) FROM CITY WHERE DISTRICT = 'California';
