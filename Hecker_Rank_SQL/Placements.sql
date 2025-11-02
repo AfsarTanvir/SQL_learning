7d4ab0478df90444ecf189de9097b4b8580a16fc
AfsarTanvir <afsartanvir805@gmail.com>
2025-11-02 20:56:46 +0600

    Placements

---
+++ Hecker_Rank_SQL/Placements.sql
@@ -0,0 +1,11 @@
+-- Hecker Rank MEdium Intermediate 
+-- https://www.hackerrank.com/challenges/placements/problem?isFullScreen=true
+-- Placements
+
+SELECT s.Name
+FROM Students s 
+JOIN Friends f ON s.ID = f.ID 
+JOIN Packages p1 ON s.ID = p1.ID 
+JOIN Packages p2 ON f.Friend_ID = p2.ID 
+WHERE p1.Salary<p2.Salary
+ORDER BY p2.Salary;
